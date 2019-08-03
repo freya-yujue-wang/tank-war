f35dbf4f37e0a305043369ea9c3b8621bc76aa37
wyjsusan <yujuewa@gmail.com>
2019-08-03 12:40:43 -0700

    version 0.5: code refactoring: use constant to define the weight and height of the window.

--- tank-war/src/com/wyjsusan/tankwar/TankClient.java
+++ tank-war/src/com/wyjsusan/tankwar/TankClient.java
@@ -3,6 +3,9 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class TankClient extends Frame {
+	
+	public static final int GAME_WIDTH = 800;
+	public static final int GAME_HEIGHT = 600;
 
 	int x = 50, y = 50;
 	
@@ -21,12 +24,12 @@ public class TankClient extends Frame {
 	@Override
 	public void update(Graphics g) {
 		if (offScreenImage == null) {
-			offScreenImage = this.createImage(800, 600);
+			offScreenImage = this.createImage(GAME_WIDTH, GAME_HEIGHT);
 		}
 		Graphics gOffScreen = offScreenImage.getGraphics();
 		Color c = gOffScreen.getColor();
 		gOffScreen.setColor(Color.GREEN);
-		gOffScreen.fillRect(0, 0, 800, 600);
+		gOffScreen.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
 		gOffScreen.setColor(c);
 		paint(gOffScreen);
 		
@@ -35,7 +38,7 @@ public class TankClient extends Frame {
 
 	public void lauchFrame() {
 		this.setLocation(400, 300);
-		this.setSize(800, 600);
+		this.setSize(GAME_WIDTH, GAME_HEIGHT);
 		this.setTitle("TankWar");
 		this.addWindowListener(new WindowAdapter() {
 
